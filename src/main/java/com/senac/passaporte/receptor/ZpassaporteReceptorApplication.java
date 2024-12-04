93b00f511be029f8f25be8e8605eaa70af34777b
LFAbrahao <lfasenac@gmail.com>
2024-12-04 16:19:05 -0300

    first commit

---
+++ src/main/java/com/senac/passaporte/receptor/ZpassaporteReceptorApplication.java
@@ -0,0 +1,13 @@
+package com.senac.passaporte.receptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpassaporteReceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpassaporteReceptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/passaporte/receptor/ZpassaporteReceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.passaporte.receptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpassaporteReceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
